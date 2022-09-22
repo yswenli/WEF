2d8c0bd509d4bc0e6fa1b9c8ed7ef3ec0841aab0
WALLE <wenguoli_520@qq.com>
2022-09-22 17:36:21 +0800

    update

--- ModelGenerator/Forms/SQLImportForm.Designer.cs
+++ ModelGenerator/Forms/SQLImportForm.Designer.cs
@@ -38,18 +38,20 @@ namespace WEF.ModelGenerator.Forms
             // 
             // skinWaterTextBox1
             // 
-            this.skinWaterTextBox1.Location = new System.Drawing.Point(54, 111);
+            this.skinWaterTextBox1.Location = new System.Drawing.Point(81, 178);
+            this.skinWaterTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.skinWaterTextBox1.Name = "skinWaterTextBox1";
-            this.skinWaterTextBox1.Size = new System.Drawing.Size(446, 25);
+            this.skinWaterTextBox1.Size = new System.Drawing.Size(667, 35);
             this.skinWaterTextBox1.TabIndex = 0;
             this.skinWaterTextBox1.WaterColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
             this.skinWaterTextBox1.WaterText = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(497, 111);
+            this.button1.Location = new System.Drawing.Point(746, 178);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(32, 25);
+            this.button1.Size = new System.Drawing.Size(48, 40);
             this.button1.TabIndex = 1;
             this.button1.Text = "..";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,9 +59,10 @@ namespace WEF.ModelGenerator.Forms
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(422, 205);
+            this.button2.Location = new System.Drawing.Point(633, 328);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(107, 42);
+            this.button2.Size = new System.Drawing.Size(160, 67);
             this.button2.TabIndex = 1;
             this.button2.Text = "导入";
             this.button2.UseVisualStyleBackColor = true;
@@ -67,19 +70,20 @@ namespace WEF.ModelGenerator.Forms
             // 
             // openFileDialog1
             // 
-            this.openFileDialog1.Filter = "Excel97|*.xls|平面文件|*.csv";
+            this.openFileDialog1.Filter = "Excel97|*.xls|平面文件|*.csv|sql文件|*.sql";
             this.openFileDialog1.Title = "导入数据";
             // 
             // SQLImportForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 280);
+            this.ClientSize = new System.Drawing.Size(888, 448);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.skinWaterTextBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "SQLImportForm";
             this.Text = "SQLImportForm";
